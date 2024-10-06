4cb30b4025baba36c1f191a5a6479690734c56f1
alxwdev <alxwdev@gmail.com>
2024-10-06 15:08:42 -0400

    Added style normalize / Added Profile component / Updated site title

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Profile from "./Profile/Profile.jsx";
+import userData from "../data/userData.json";
+
+function App() {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+        style={{ margin: "0 auto" }}
+      />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,37 @@
+import css from "./Profile.module.css";
+
+export default function Profile({
+  name,
+  tag,
+  location,
+  image = "https://cdn-icons-png.flaticon.com/512/1077/1077012.png",
+  stats,
+  style,
+}) {
+  const { followers, views, likes } = stats;
+  return (
+    <div className={css.container} style={style}>
+      <div className={css.thumb}>
+        <img className={css.image} src={image} alt="User avatar" />
+        <p className={css.name}>{name}</p>
+        <p className={css.description}>@{tag}</p>
+        <p className={css.description}>{location}</p>
+      </div>
+
+      <ul className={css.list}>
+        <li className={css.item}>
+          <span>Followers</span>
+          <span className={css.value}>{followers}</span>
+        </li>
+        <li className={css.item}>
+          <span>Views</span>
+          <span className={css.value}>{views}</span>
+        </li>
+        <li className={css.item}>
+          <span>Likes</span>
+          <span className={css.value}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
